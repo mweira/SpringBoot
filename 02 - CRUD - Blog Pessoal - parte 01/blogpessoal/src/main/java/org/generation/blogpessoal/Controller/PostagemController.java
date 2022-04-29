b67d7103424840fd8bebc9ab2b9d58532c9d0657
João Pedro <jpmeira020@gmail.com>
2022-04-29 09:58:15 -0300

    Commit 3

---
+++ 02 - CRUD - Blog Pessoal - parte 01/blogpessoal/src/main/java/org/generation/blogpessoal/Controller/PostagemController.java
@@ -0,0 +1,27 @@
+package org.generation.blogpessoal.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.generation.blogpessoal.Model.Postagem;
+import org.generation.blogpessoal.Repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
